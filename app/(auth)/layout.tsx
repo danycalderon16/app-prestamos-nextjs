e188b05dcccf92113990160ee6fa1503232d9bf3
Daniel Calderon <daniel.calderon@mxmart.mx>
2023-11-24 23:24:23 -0700

    Sign in desing complete

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LayoutAuth = ({children}:React.PropsWithChildren) => {
+  return (
+    <div className='h-full w-full'>{
+children
+    }</div>
+  )
+}
+
+export default LayoutAuth
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,26 +1,59 @@
-"use client"
+"use client";
 
 import { UserAuth } from "@/context/AuthContext";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 import React, { useEffect } from "react";
 export default function Page() {
-
-  const {user, googleSignIn} = UserAuth()
+  const { user, googleSignIn } = UserAuth();
   const showPopUp = () => {
     try {
       googleSignIn();
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    if(user){
-      redirect("/loans")
-    }   
-  }, [user])
-  
-  return <div className="flex">
-    <button onClick={showPopUp}>Sign in</button>
-  </div>;
+    if (user) {
+      redirect("/loans");
+    }
+  }, [user]);
+
+  return (
+    <div className="bg-white flex h-full">
+      <div className="flex items-center justify-center h-screen w-screen dark:bg-gray-800 ">
+        <button
+        onClick={showPopUp}
+          className={`px-4
+                py-2
+                border
+                flex
+                gap-2
+                border-slate-200
+                dark:border-slate-700
+                rounded-lg
+                text-slate-700
+                dark:text-slate-200
+                hover:border-slate-400
+                dark:hover:border-slate-500
+                hover:text-slate-900
+                dark:hover:text-slate-300
+                hover:shadow
+                transition
+                duration-150`}
+        >
+          <Image
+            className="w-6 h-6"
+            src={"/google_logo.svg"}
+            loading="lazy"
+            width={24}
+            height={24}
+            alt="google logo"
+          />
+          <span>Login with Google</span>
+        </button>
+      </div>
+    </div>
+  );
 }
