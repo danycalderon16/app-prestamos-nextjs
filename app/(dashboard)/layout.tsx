f6e1a79d42df72a24e796eac9d4b634c942ba2b3
Daniel Calderon <danycalvir@gmail.com>
2024-01-26 23:58:10 -0700

    navbar responsive

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import SideMenu from "@/components/side-menu";
 import { BottomMenu } from "@/components/bottom-menu";
+import Navbar from "@/components/navbar";
 export default async function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="ml-64 h-full">
+    <div className="sm:ml-64 h-full">
+      <Navbar/>
       {children}
       <SideMenu />
       <BottomMenu />
