03652bd05b1ea3070eedaf255ec0c4196318c78c
Daniel Calderon <danycalvir@gmail.com>
2024-02-09 22:19:27 -0700

    add option

--- app/(dashboard)/loans/[id]/components/info-loan.tsx
+++ app/(dashboard)/loans/[id]/components/info-loan.tsx
@@ -1,5 +1,12 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { Loan } from "@/interfaces/loans";
+import { CheckCircle, MoreHorizontal, Trash2 } from "lucide-react";
 interface Props {
   loan: Loan;
 }
@@ -47,7 +54,35 @@ export default function InfoLoan({ loan }: Props) {
       <div className="grid grid-cols-3 grid-rows-2 items-center place-items-center mt-4 ">
         <span className="">Anbonos</span>
         <span className="">Tipo</span>
-        <Button className="row-span-2" variant={"outline"}>Añadir</Button>
+        <div className="row-span-2">
+          <Popover>
+            <PopoverTrigger>
+              <Button variant={"outline"}>
+                <MoreHorizontal className="text-gray-600" size={30} />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent>
+            <button
+              onClick={() =>{}}
+              className={`flex items-center p-2 text-gray-900 rounded-lg`}
+            >
+              <CheckCircle className="text-gray-500" size={20} />
+              <span className="flex-1 ms-3 whitespace-nowrap">
+                Marcar como completado
+              </span>
+            </button>
+            <button
+              onClick={() => {}}
+              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+            >
+              <Trash2 className="text-gray-500" size={20} />
+              <span className="flex-1 ms-3 whitespace-nowrap">
+                Eliminar prestamo
+              </span>
+            </button>
+            </PopoverContent>
+          </Popover>
+        </div>
         <span className="font-light">
           {loan.abonos}/{loan.plazos}
         </span>
